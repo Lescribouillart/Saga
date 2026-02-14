37d186c5b2883da42a812454b7feac056cdc000c
Drelall <137816034+Drelall@users.noreply.github.com>
2026-02-14 11:23:37 +0100

    maj

--- javascript/character-display.js
+++
@@ -1,5 +0,0 @@
-// Fichier character-display.js - Simplifié
-// Toutes les fonctions de création de personnage ont été supprimées
-// Le jeu se lance maintenant directement au chargement de la page
-
-console.log('character-display.js chargé - Mode jeu direct activé');
